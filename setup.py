d5f3248415fe46c9b28298acdd8bb008eae4086b
Mia Chono <mia.chono@protonmail.com>
2022-06-06 21:01:10 +0200

    update requirements and setup

--- setup.py
+++ setup.py
@@ -2,29 +2,24 @@ import os
 
 import setuptools
 
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name="crunchy_api",
     version="0.0.1",
     author="Mia Chono",
-    description=("A Python API for Crunchyroll (beta)."),
+    description="A Python API for Crunchyroll (beta).",
     license="MIT",
     url="https://github.com/mia-chono/crunchy_api",
     install_requires=[
         "requests",
     ],
-    long_description=read('README'),
+    long_description=long_description,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
     python_requires=">=3.9",
