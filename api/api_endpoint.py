ebbd5fe3f6fd1265c0d66564fede4a01a161d93b
mia-chono <mia.chono@protonmail.com>
2022-06-06 17:29:17 +0200

    add objects endpoint

--- api/api_endpoint.py
+++ api/api_endpoint.py
@@ -4,6 +4,7 @@ class ApiEndpoint:
     PROFILE = "https://beta-api.crunchyroll.com/accounts/v1/me/profile"
     SEARCH = "https://beta-api.crunchyroll.com/content/v1/search"
     INDEX = "https://beta-api.crunchyroll.com/index/v2"
+    OBJECTS = "https://beta-api.crunchyroll.com/cms/v2{bucket}/objects/{id}"
     STREAMS = "https://beta-api.crunchyroll.com/cms/v2{bucket}/videos/{id}/streams"
     SERIES = "https://beta-api.crunchyroll.com/cms/v2{bucket}/series/{id}"
     SEASONS = "https://beta-api.crunchyroll.com/cms/v2{bucket}/seasons"
