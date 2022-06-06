b0a4f3c257b665e3fbc1cb4298521e3fab94552c
mia-chono <mia.chono@protonmail.com>
2022-06-06 12:48:23 +0200

    add api endpoints

---
+++ api/api_endpoint.py
@@ -0,0 +1,10 @@
+class ApiEndpoint:
+    TOKEN = "https://beta-api.crunchyroll.com/auth/v1/token"
+    ME = "https://beta-api.crunchyroll.com/accounts/v1/me"
+    PROFILE = "https://beta-api.crunchyroll.com/accounts/v1/me/profile"
+    SEARCH = "https://beta-api.crunchyroll.com/content/v1/search"
+    INDEX = "https://beta-api.crunchyroll.com/index/v2"
+    STREAMS = "https://beta-api.crunchyroll.com/cms/v2{bucket}/videos/{id}/streams"
+    SERIES = "https://beta-api.crunchyroll.com/cms/v2{bucket}/series/{id}"
+    SEASONS = "https://beta-api.crunchyroll.com/cms/v2{bucket}/seasons"
+    EPISODES = "https://beta-api.crunchyroll.com/cms/v2{bucket}/episodes"
