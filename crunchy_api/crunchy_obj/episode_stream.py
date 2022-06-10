21159f821447fad2c37d7316395867b74932c9d6
Mia Chono <mia.chono@protonmail.com>
2022-06-10 22:31:35 +0200

    fix episode stream object after tests

--- crunchy_api/crunchy_obj/episode_stream.py
+++ crunchy_api/crunchy_obj/episode_stream.py
@@ -8,12 +8,12 @@ from crunchy_api.crunchy_obj.subtitles import Subtitle
 class EpisodeStream(BaseCrunchyrollObjectInterface):
     def __init__(self, data_source: Optional[dict] = {}):
         self.audio_locale: Optional[str] = data_source.get("audio_locale")
-        self.subtitles: list[dict[str, Subtitle]] = [{key: Subtitle(value)} for key, value in data_source.get("subtitles", {}).items()]
+        self.subtitles: dict[str, Subtitle] = {key: Subtitle(value) for key, value in data_source.get("subtitles", {}).items()}
         self.streams: Streams = Streams(data_source.get("streams", {}))
         self.bifs: list[str] = data_source.get("bifs", [])
 
     def load_data_source(self, data_source: dict):
         self.audio_locale = data_source.get("audio_locale") or self.audio_locale
-        self.subtitles = [{key: Subtitle(value)} for key, value in data_source.get("subtitles", {}).items()]
+        self.subtitles = {key: Subtitle(value) for key, value in data_source.get("subtitles", {}).items()}
         self.streams.load_data_source(data_source.get("streams", {}))
         self.bifs = data_source.get("bifs", []) or self.bifs
